fca1c4b17e5b195b4bfd29577d038df5f4fcbccb
stvpormvndi <f.lumachini@gmail.com>
2023-05-30 18:48:02 +0200

    Updated a lot of the code. The only missing functionality is the possibility to update the items

--- script.js
+++ script.js
@@ -25,50 +25,53 @@ function deleteItem(e) {
     e.currentTarget.removeEventListener("click", deleteItem);
     e.currentTarget.parentElement.remove();
     toggleHiddenClass(document.getElementById("items-list"));
+    updateLocalStorage();
+    document.querySelector("#item-input").value = "";
 }
 
-itemForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const currentItem = document.querySelector("#item-input");
+function updateLocalStorage() {
+    localStorage.clear();
+    localStorage.setItem("storedItems", JSON.stringify(currentItemsArray));
+}
+
+// The function that creates the items
+function createItem(item) {
+    /* const currentItem = document.querySelector("#item-input"); */
     const itemDiv = document.createElement("div");
     itemDiv.setAttribute("class", "item-container");
     const itemP = document.createElement("p");
-    itemP.innerText = `${currentItem.value.charAt(0).toUpperCase() + currentItem.value.slice(1)}`.trim();
-    currentItemsArray.push(currentItem.value.toLowerCase().trim());
+    itemP.innerText = `${item.charAt(0).toUpperCase() + item.slice(1)}`.trim();
+    currentItemsArray.push(item.toLowerCase().trim());
     console.log(currentItemsArray);
     const itemIcon = document.createElement("i");
     itemIcon.setAttribute("class", "fa-solid fa-x");
     itemDiv.appendChild(itemP);
     itemDiv.appendChild(itemIcon);
     itemsListSection.appendChild(itemDiv);
-    currentItem.value = "";
+    document.querySelector("#item-input").value = "";
     toggleHiddenClass(document.getElementById("items-list"));
-    let xButtons = document.querySelectorAll(".fa-x");
-    xButtons.forEach((element) => {
-        element.removeEventListener("click", deleteItem);
-    });
-    xButtons.forEach((element) => {
-        /* element.addEventListener("click", function deleteItem(e) {
-            e.stopPropagation();
-            let itemToRemoveFromStorage = e.currentTarget.previousElementSibling.innerText.toLowerCase().trim();
-            let indexOfItemToRemove = currentItemsArray.indexOf(itemToRemoveFromStorage);
-            console.log(itemToRemoveFromStorage);
-            console.log(indexOfItemToRemove);
-            currentItemsArray.splice(indexOfItemToRemove, 1);
-            console.log(currentItemsArray);
-            e.currentTarget.removeEventListener("click", deleteItem);
-            e.currentTarget.parentElement.remove();
-            toggleHiddenClass(document.getElementById("items-list"));
-        }); */
-        element.addEventListener("click", deleteItem);
-    });
-    const currentItems = document.querySelectorAll(".item-container");
-    currentItems.forEach((element)=>{
-        element.addEventListener("click", (e) => {
-            e.stopPropagation();
-            currentItem.value = e.currentTarget.firstChild.innerText;
-        })
+    itemIcon.addEventListener("click", deleteItem);
+    itemDiv.addEventListener("click", (e) => {
+        e.stopPropagation();
+        document.querySelector("#item-input").value = e.currentTarget.firstChild.innerText;
     })
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    const persistingItems = JSON.parse(localStorage.getItem("storedItems"));
+    persistingItems.forEach((item) => {
+        createItem(item);
+    });
+})
+
+itemForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (!currentItemsArray.includes(document.querySelector("#item-input").value.toLowerCase())) {
+        createItem(document.querySelector("#item-input").value);
+    } else {
+        document.querySelector("#item-input").value = "";
+    }
+    updateLocalStorage();
 })
 
 
@@ -77,6 +80,8 @@ clearButton.addEventListener("click", () => {
     itemsListSection.innerHTML = "";
     toggleHiddenClass(document.getElementById("items-list"));
     currentItemsArray = [];
+    updateLocalStorage();
+    document.querySelector("#item-input").value = "";
 })
 
 // The filter form
